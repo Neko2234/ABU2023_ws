b7b375d6e3ddee610c0a307b4d2e6ef4175bc71c
Neko2234 <biwakocatfish@gmail.com>
2023-04-10 15:57:44 +0900

    add launch file

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/m/ABU2023_ws/src
-# Build directory: /home/m/ABU2023_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("load_ring_package")
--- build/load_ring_package/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/m/ABU2023_ws/src/load_ring_package
-# Build directory: /home/m/ABU2023_ws/build/load_ring_package
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
