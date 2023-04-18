77370abbd1e6fedbd963fbe45dc1cbbedb4853f1
Neko2234 <biwakocatfish@gmail.com>
2023-04-18 19:25:08 +0900

    ROSコントローラ対応

--- Arduino_sketch/separate_rings/separate_rings.ino
+++ Arduino_sketch/separate_rings/separate_rings.ino
@@ -2,66 +2,57 @@
 #include <ros.h>
 #include <adbot_msgs/SprMsg.h>
 
-#define SPR_MOTOR 8  // 分離のモーター番号
-#define STOP 0
-#define CW 1
-#define CCW 2
-#define ONE_WAY_TIME 3000  //ミリ秒
-#define STOP_TIME 1000
+#define SPR_MOTOR 8    // 分離のモーター番号
+#define SPR_ENC_NUM 0  // 分離のエンコーダ番号
+#define ONE_WAY_COUNT 500  //片道のエンコーダカウント
+#define STOP_COUNT 1000
+#define ENC_DIFF_MIN 5
 
-int spr_duty = 800;
-unsigned long time_prev = 0;
-unsigned long time_now = 0;
-unsigned long dt = 0;
-unsigned long corrected_dt = 0;
+int spr_duty = 50;   //指定するDutyの絶対値ROSメッセージから指定できる
+int duty = spr_duty;  //実際にCubicに指定する値
+
 bool separate_sign = false;
 bool separate_pre_sign = false;
 bool is_separating = true;
-bool is_pausing = false;
-unsigned long pause_start_time = 0;
-unsigned long pause_end_time = 0;
-unsigned long pause_time = 0;
+bool is_paused = true;
+
+bool is_checking = false;
+unsigned long check_time = 0;
+unsigned long time_start = 0;
+unsigned long time_now = 0;
+
 
 ros::NodeHandle nh;
 
-// トピックのコールバック関数
+トピックのコールバック関数
 void separateRingCallback(const adbot_msgs::SprMsg &spr_msg) {
   separate_pre_sign = separate_sign;
   separate_sign = spr_msg.isOn;
   spr_duty = spr_msg.duty;
 }
 
-// トピックを受け取るためのサブスクライバーを作成
+トピックを受け取るためのサブスクライバーを作成
 ros::Subscriber<adbot_msgs::SprMsg> sub("separate", &separateRingCallback);
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(22, OUTPUT);
+  pinMode(24, OUTPUT);
   pinMode(23, OUTPUT);
+
   // すべてのモータ，エンコーダの初期化
-  Cubic::begin();
+  Cubic::begin(3.0);
+  Inc_enc::reset();
   nh.getHardware()->setBaud(9600);
 
-  // ROSの通信を開始
+  ROSの通信を開始
   nh.initNode();
   nh.subscribe(sub);
 }
 
 void loop() {
-  nh.spinOnce();
-  delay(30);
-  Serial.print(time_now);
-  Serial.print(",");
-  Serial.print(time_prev);
-  Serial.print(",");
-  Serial.print(pause_time);
-  Serial.print(",");
-  Serial.print(dt);
-  Serial.print(",");
-  Serial.print(corrected_dt);
-  Serial.print(",");
-  Serial.println(is_separating);
+  int16_t enc_diff = Inc_enc::get_diff(SPR_ENC_NUM);
+  int32_t enc_count = Inc_enc::get(SPR_ENC_NUM);
 
+  nh.spinOnce();
 
   // 立ち上がり(スイッチを押した瞬間)で分離実行を切り替え
   if (separate_sign && !separate_pre_sign) {
@@ -69,37 +60,24 @@ void loop() {
   }
 
   if (is_separating) {
-    digitalWrite(23, LOW);
-    time_now = millis();
 
-    if (!is_pausing) {
-      pause_end_time = time_now;
-      is_pausing = false;
-    }
-    pause_time = pause_end_time - pause_start_time;
-
-    dt = time_now - time_prev;
-    corrected_dt = dt - pause_time; // pausetime is bad
-
-    if (corrected_dt < ONE_WAY_TIME) {
-      pause_time = 0;
-      digitalWrite(22, LOW);
-      DC_motor::put(SPR_MOTOR, spr_duty);
-    } else if (corrected_dt < ONE_WAY_TIME + STOP_TIME) {
-      DC_motor::put(SPR_MOTOR, 0);
-    } else if (corrected_dt < ONE_WAY_TIME * 2 + STOP_TIME) {
-      digitalWrite(22, LOW);
-      DC_motor::put(SPR_MOTOR, -spr_duty);
-    } else if (corrected_dt < ONE_WAY_TIME * 2 + STOP_TIME * 2) {
-      DC_motor::put(SPR_MOTOR, 0);
-    } else {
-      time_prev = time_now;
+    digitalWrite(23, HIGH);
+    if (enc_count > ONE_WAY_COUNT) {
+      digitalWrite(24, LOW);
+      duty = spr_duty;
+    } else if (enc_count < 0) {
+      digitalWrite(24, HIGH);
+      duty = -spr_duty;
+    } else if (is_paused) {
+      duty = spr_duty;
+      is_paused = false;
     }
   } else {
-    digitalWrite(23, HIGH);
-    is_pausing = true;
-    pause_start_time = time_now;
+    digitalWrite(23, LOW);
+    is_paused = true;
+    duty = 0;
   }
+  DC_motor::put(SPR_MOTOR, duty);
 
   // データの送受信を行う
   Cubic::update();
